ddff7421a3f955ce0a6769e265625da571dc6d27
Jin Kim <wlsgns92@hotmail.com>
2020-07-28 16:10:53 +1200

    add and delete todo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import { render } from '@testing-library/react';
 
 class App extends Component {
 
   constructor(props) {
     super(props)
 
-    this.notes = [
-      {
-        id: 1,
-        text: 'watch youtube',
-      },
-      {
-        id: 2,
-        text: 'watch netflix',
-      },
-      {
-        id: 3,
-        text: 'do nothing',
-      },
-      {
-        id: 4,
-        text: 'do react',
-      },
-      {
-        id: 5,
-        text: 'practice git',
-      }
-    ]
+    this.state = {
+      notes: [
+        {
+          id: 1,
+          text: 'watch youtube',
+        },
+        {
+          id: 2,
+          text: 'watch netflix',
+        },
+        {
+          id: 3,
+          text: 'do nothing',
+        },
+        {
+          id: 4,
+          text: 'do react',
+        },
+        {
+          id: 5,
+          text: 'practice git',
+        }
+      ],
+
+      noteInputValue: ''
+    }
+
+
+  }
+
+  handleNoteInputChange = (e) => {
+    this.setState({ noteInputValue: e.target.value })
+    console.log(e.target.value)
+
+  }
+
+  handleAddButton = (e) => {
+    e.preventDefault()
+    console.log(this.state.notes.length)
+    
+    let note = {
+      // id:Date.now(),
+      id: this.state.notes.length+1,
+      text: this.state.noteInputValue,
+    }
+
+    let newNotes = [note,...this.state.notes]
+
+    this.setState({
+      notes:newNotes,
+      noteInputValue:'',
+    })
+  }
+
+  handleNoteDelete = (e) =>{
+    let noteIdToDelete = parseInt(e.target.id)
+    let notes = this.state.notes
+
+    
+
+    let filteredNotes = notes.filter((item)=>{
+      return item.id !== noteIdToDelete
+    })
+    console.log({filteredNotes})
+
+    this.setState({notes:filteredNotes})
   }
 
   render() {
@@ -37,11 +82,11 @@ class App extends Component {
         <div className="container">
           <div className="notes">
             {
-              this.notes.map(function(note){
-                return(
-                  <div className="note">
+              this.state.notes.map( (note)=>{
+                return (
+                  <div className="note" key={note.id}>
                     <div className="note-body">
-                      <i className="far fa-times-circle note-remove"></i>
+                      <i id={note.id} className="far fa-times-circle note-remove" onClick={this.handleNoteDelete}></i>
                       <div className="note-text">
                         {note.text}
                       </div>
@@ -56,17 +101,17 @@ class App extends Component {
               <form className="note-body">
                 <div className="form-group">
                   <label htmlFor="note-input">New note</label>
-                  <input type="text" className="form-control" id="note-input"></input>
+                  <input type="text" className="form-control" id="note-input" value={this.state.noteInputValue} onChange={this.handleNoteInputChange}></input>
                 </div>
 
-                <button type="submit" className="btn btn-primary">Add</button>
+                <button type="submit" className="btn btn-primary" onClick={this.handleAddButton}>Add</button>
               </form>
 
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 
 }
